1430bf224a915f3d7fc5360f254dbc28b29dbdbd
Momme Jürgensen <momme@juergensen.me>
2021-04-26 19:05:00 +0000

    removed some helper

--- shared-resources/helpers.js
+++ shared-resources/helpers.js
@@ -1,27 +1,7 @@
-function canReadService(session, service) {
-  return session.permissions.some(perm => perm.action === 'read' && perm.service.toString() == service.toString());
-}
-
-function canCreateService(session, service) {
-  return session.permissions.some(perm => perm.action === 'create' && perm.service.toString() == service.toString());
-}
-
-function canPatchService(session, service) {
-  return session.permissions.some(perm => perm.action === 'patch' && perm.service.toString() == service.toString());
-}
-
-function canRemoveService(session, service) {
-  return session.permissions.some(perm => perm.action === 'remove' && perm.service.toString() == service.toString());
-}
-
 function hasScenario(session, scenarioId = '') {
   return session.scenarioIds.some(id => id.toString == scenarioId.toString());
 }
 
 module.exports = {
-  canReadService,
-  canCreateService,
-  canPatchService,
-  canRemoveService,
   hasScenario
 };
